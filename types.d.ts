dbfa4331916069f9c4b97bd370a782f22c362083
Edward <e.arechwa@gmail.com>
2024-05-17 12:25:21 +0200

    ft: add right types

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,36 @@
-type User = {
-  id: string;
+type Geo = {
+  lat: string;
+  lng: string;
+};
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+type Company = {
   name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
 };
 
 type Post = {
-  id: string;
+  userId: number;
+  id: number;
   title: string;
   body: string;
 };
